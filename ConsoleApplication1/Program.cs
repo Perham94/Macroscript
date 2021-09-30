76526c99282590040ce194f6de770b9fee2b1f45
Perham <perham132@hotmail.se>
2021-09-30 21:08:36 +0200

    added test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(
+            string lpClassName, // class name 
+            string lpWindowName // window name 
+        );
+
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(
+            int hWnd // handle to window
+        );
+
+
+
+        public static void SendKeys()
+        {
+            
+            int iHandle = FindWindow(null, "New World");
+            SetForegroundWindow(iHandle);
+           
+          
+
+      
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key != ConsoleKey.Escape))
+            {
+                if (DateTime.Now.Hour >= 21)
+                {
+                    break;
+                }
+                
+        
+                    Keyboard.SendKey(Keyboard.DirectXKeyStrokes.DIK_TAB, false, Keyboard.InputType.Keyboard);
+                    Thread.Sleep(500);
+
+            } 
+        }
+        
+        
+        public static void Main(string[] args)
+        
+        {
+            SendKeys();
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Properties/test.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace WindowsFormsApp1.Properties
+{
+    public partial class test : Component
+    {
+        public test()
+        {
+            InitializeComponent();
+        }
+
+        public test(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
