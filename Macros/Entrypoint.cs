74c3395c5467dbdaa9d373cbcf14b8884a29a275
Perham <perham132@hotmail.se>
2021-10-02 21:01:11 +0200

    non pattern movement

---
+++ Macros/Entrypoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace ConsoleApplication1
+{
+    class Entrypoint
+
+    {
+        public static void Main(string[] args)
+        {
+            InputSimulator Input = new InputSimulator();
+            Keyboard.SendKeys(Input);
+        }
+    }
+}
---
+++ Macros/Keyboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace ConsoleApplication1
+{
+    internal class Keyboard
+    {
+        private static int i { get; set; }
+
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(
+            string lpClassName,
+            string lpWindowName
+        );
+
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(
+            int hWnd
+        );
+
+        public static void SendKeys(InputSimulator input)
+        {
+            int iHandle = FindWindow(null, "New World");
+            SetForegroundWindow(iHandle);
+
+
+            do
+            {
+                Random rnd = new Random();
+                i = rnd.Next(0, 3);
+                input.Keyboard.KeyDown(GetKey(i));
+                Thread.Sleep(500);
+                input.Keyboard.KeyUp(GetKey(i));
+                // input.Mouse.LeftButtonClick();Thread.Sleep(500);
+
+                input.Keyboard.KeyUp(VirtualKeyCode.VK_W);
+            } while (!input.InputDeviceState.IsKeyDown(VirtualKeyCode.ESCAPE));
+        }
+
+
+        public static VirtualKeyCode GetKey(int Index)
+        {
+            var list = new[] {VirtualKeyCode.VK_A, VirtualKeyCode.VK_D, VirtualKeyCode.VK_S, VirtualKeyCode.VK_W};
+            return list[Index];
+        }
+    }
+}
